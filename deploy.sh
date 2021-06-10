94b4f0c378e0f7a1f96621da13584ccdbec471d9
nischal shrestha <nischalstha9@gmail.com>
2021-06-10 18:33:32 +0545

    media try

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,15 @@ pkill -f uwsgi -9
 mkdir expensetracker
 cd ./expensetracker
 git clone https://github.com/nischalstha9/ExpenseTracker .
-git pull
 git pull origin master
-pipenv shell
-pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+mkdir DockerData/media
+cp ./media DockerData/media
+# pipenv shell
+# pip install -r requirements.txt
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py collectstatic --noinput
+
 docker-compose up --build -d
 docker image prune -a --filter "until=24h" -f
 exit
