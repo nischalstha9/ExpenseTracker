7cfd11314e5ebc5fae68edc015c4d700087e567b
nischal shrestha <nischalstha9@gmail.com>
2022-05-08 08:20:01 +0545

    added url paths and staticfiles settings

--- project/urls.py
+++ project/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static, serve
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_swagger.views import get_swagger_view
+from django.conf.urls import url
+from transaction.views import home
+
 
 
 schema_view = get_swagger_view(title='Donation Platform API')
@@ -28,7 +32,29 @@ urlpatterns = [
     path('api/v1/expensetracker/', include('transaction.urls'), name="ExpenseTrackerAPI"),
     path('swagger/', schema_view, name="SwaggerView"),
     path('__debug__/', include('debug_toolbar.urls')),
+    # url(r'^media/(?P<path>.*)$', serve,
+    #     {'document_root': settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve,
+    #     {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+react_views = [
+    # List all your react routes here
+    "",
+    "logout/",
+    "register/",
+    "login/",
+    "accounts/",
+    "manage/accounts/",
+    "manage/accounts/<int:id>/",
+    "create-account-book/",
+    "accounts/<int:id>/",
+    "profile/",
+    "change-password/",
+]
+
+urlpatterns += [path(i, home, name='homepage') for i in react_views]
