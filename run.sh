0381d5b5138f0cd25f87fbc48a4a99d7660a7bb3
Nischal Shrestha <nischalstha9@gmail.com>
2022-10-14 10:40:19 +0545

    listen on 0.0.0.0

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-uwsgi --http :8002 --auto-procname --master --module=project.wsgi:application --enable-threads --processes 4 --cheaper 2 --cheaper-step 1 --listen=100 --async 4 --harakiri 60 --max-requests 1000 --max-worker-lifetime 3600 --reload-on-rss 1024 --worker-reload-mercy 60
+uwsgi --http 0.0.0.0:8002 --auto-procname --master --module=project.wsgi:application --enable-threads --processes 4 --cheaper 2 --cheaper-step 1 --listen=100 --async 4 --harakiri 60 --max-requests 1000 --max-worker-lifetime 3600 --reload-on-rss 1024 --worker-reload-mercy 60
