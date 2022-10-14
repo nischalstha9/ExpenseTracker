b809343bd91bf9598231de235d59213523da07b1
Nischal Shrestha <nischalstha9@gmail.com>
2022-10-14 19:44:36 +0545

    back to gunicorn

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-uwsgi --http 0.0.0.0:8002 --auto-procname --master --module=project.wsgi:application --enable-threads --processes 4 --cheaper 2 --cheaper-step 1 --listen=100 --async 4 --harakiri 60 --max-requests 1000 --max-worker-lifetime 3600 --reload-on-rss 1024 --worker-reload-mercy 60
+gunicorn project.wsgi --bind 0.0.0.0:8000
+#uwsgi --http 0.0.0.0:8000 --auto-procname --master --module=project.wsgi:application --enable-threads --processes 4 --cheaper 2 --cheaper-step 1 --listen=100 --async 4 --harakiri 60 --max-requests 1000 --max-worker-lifetime 3600 --reload-on-rss 1024 --worker-reload-mercy 60
